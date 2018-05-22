999f933971058de3e16197bbd67c235bc0942ecc
Hendi Santika <hendisantika@gmail.com>
2018-05-23 05:16:04 +0700

    #2 - Second Commit

---
+++ src/main/java/com/hendisantika/springbootquerydsl/SpringbootQuerydslApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuerydslApplication.class, args);
+    }
+}
