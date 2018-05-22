d360935bbf70a71adb8f534884e15913d641c60c
Hendi Santika <hendisantika@gmail.com>
2018-05-23 05:32:08 +0700

    #7 - Add QDepartment Model

---
+++ src/main/java/com/hendisantika/springbootquerydsl/model/QDepartment.java
@@ -0,0 +1,39 @@
+package com.hendisantika.springbootquerydsl.model;
+
+import com.mysema.query.types.Path;
+import com.mysema.query.types.PathMetadata;
+import com.mysema.query.types.path.EntityPathBase;
+import com.mysema.query.types.path.NumberPath;
+import com.mysema.query.types.path.StringPath;
+
+import javax.annotation.Generated;
+
+import static com.mysema.query.types.PathMetadataFactory.forVariable;
+
+
+/**
+ * QDepartment is a Querydsl query type for Department
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QDepartment extends EntityPathBase<Department> {
+
+    public static final QDepartment department = new QDepartment("department");
+    private static final long serialVersionUID = -848637883L;
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public QDepartment(String variable) {
+        super(Department.class, forVariable(variable));
+    }
+
+    public QDepartment(Path<? extends Department> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QDepartment(PathMetadata<?> metadata) {
+        super(Department.class, metadata);
+    }
+
+}
+
