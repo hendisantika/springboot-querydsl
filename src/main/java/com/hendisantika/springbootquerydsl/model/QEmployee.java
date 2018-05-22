f97afa25f7f483cd7e4736d422c5b77b8b0a3209
Hendi Santika <hendisantika@gmail.com>
2018-05-23 05:31:46 +0700

    #6 - Add QEmployee Model

---
+++ src/main/java/com/hendisantika/springbootquerydsl/model/QEmployee.java
@@ -0,0 +1,54 @@
+package com.hendisantika.springbootquerydsl.model;
+
+import com.mysema.query.types.Path;
+import com.mysema.query.types.PathMetadata;
+import com.mysema.query.types.path.EntityPathBase;
+import com.mysema.query.types.path.NumberPath;
+import com.mysema.query.types.path.PathInits;
+import com.mysema.query.types.path.StringPath;
+
+import javax.annotation.Generated;
+
+import static com.mysema.query.types.PathMetadataFactory.forVariable;
+
+
+/**
+ * QEmployee is a Querydsl query type for Employee
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QEmployee extends EntityPathBase<Employee> {
+
+    public static final QEmployee employee = new QEmployee("employee");
+    private static final long serialVersionUID = -1181477791L;
+    private static final PathInits INITS = PathInits.DIRECT2;
+    public final QDepartment department;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<java.math.BigDecimal> salary = createNumber("salary", java.math.BigDecimal.class);
+
+    public QEmployee(String variable) {
+        this(Employee.class, forVariable(variable), INITS);
+    }
+
+    public QEmployee(Path<? extends Employee> path) {
+        this(path.getType(), path.getMetadata(), path.getMetadata().isRoot() ? INITS : PathInits.DEFAULT);
+    }
+
+    public QEmployee(PathMetadata<?> metadata) {
+        this(metadata, metadata.isRoot() ? INITS : PathInits.DEFAULT);
+    }
+
+    public QEmployee(PathMetadata<?> metadata, PathInits inits) {
+        this(Employee.class, metadata, inits);
+    }
+
+    public QEmployee(Class<? extends Employee> type, PathMetadata<?> metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.department = inits.isInitialized("department") ? new QDepartment(forProperty("department")) : null;
+    }
+
+}
+
