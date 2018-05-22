19a8ffa8c59889e8ef2838c26e1d46bb3d8799ef
Hendi Santika <hendisantika@gmail.com>
2018-05-23 05:17:58 +0700

    #4 - Add Department Model

---
+++ src/main/java/com/hendisantika/springbootquerydsl/model/Department.java
@@ -0,0 +1,44 @@
+package com.hendisantika.springbootquerydsl.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-querydsl
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/05/18
+ * Time: 05.17
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
