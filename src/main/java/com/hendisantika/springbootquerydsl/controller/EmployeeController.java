f5cd316b0fb04156078ee649614e0b9ae4410d89
Hendi Santika <hendisantika@gmail.com>
2018-05-25 05:17:14 +0700

    #12 - Beautify Controller

--- src/main/java/com/hendisantika/springbootquerydsl/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootquerydsl/controller/EmployeeController.java
@@ -27,37 +27,37 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+    @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee.getName(), employee.getSalary(), employee.getDepartment());
     }
 
-    @RequestMapping(value = "/departments", method = RequestMethod.POST)
+    @GetMapping("/departments")
     public Department createDepartment(@RequestBody Department department) {
         return employeeService.saveDepartment(department.getName());
     }
 
-    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.getEmployees();
     }
 
-    @RequestMapping(value = "/employeeNames", method = RequestMethod.GET)
+    @GetMapping("/employeeNames")
     public List<String> getEmployeeNames() {
         return employeeService.getEmployeeNames();
     }
 
-    @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
+    @GetMapping("/employees/{id}")
     public Employee getEmployeeById(@PathVariable long id) {
         return employeeService.getEmployeeById(id);
     }
 
-    @RequestMapping(value = "/employees/by_salary", method = RequestMethod.GET)
+    @GetMapping("/employees/by_salary")
     public List<Employee> getEmployeesBySalary(@RequestParam BigDecimal minSalary, @RequestParam BigDecimal maxSalary) {
         return employeeService.getEmployeesWithSalaryBetween(minSalary, maxSalary);
     }
 
-    @RequestMapping(value = "/employees/by_department_name", method = RequestMethod.GET)
+    @GetMapping("/employees/by_department_name")
     public List<Employee> getEmployeesByDepartmentName(@RequestParam String name) {
         return employeeService.getEmployeesByDepartmentName(name);
     }
